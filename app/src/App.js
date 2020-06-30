5f164f47e056428811cd61a478e7bf3114c2587f
HoussemEddine <housse-eddine.lmasoudi@enis.tn>
2020-06-30 12:48:52 +0100

    initial

---
+++ app/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   
+  <div class='container'>
+    <form class='container'>
+      <h1>React + bootstrapForm</h1>
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+   <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" ></input>
+</div>
+     <div class='form-group'>
+
+     <label for="exampleInputPassword1">Password</label>
+     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+
+     </div>
+     <div class='form-group'>
+     <label for="exampleInputEmail1">Phone Number</label>
+            <input type="number" class="form-control" placeholder="Phone Number"></input>
+     </div>
+     <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+            <label class="form-check-label" for="exampleCheck1">Check</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+     </form>
+  </div>
+    
+  );
+}
+
+export default App;
